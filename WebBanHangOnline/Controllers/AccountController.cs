cdc7cbe1579150538f83edd0a728d9ff17612958
Phuc Toan <Lenovo@LAPTOP-DT77IDDO>
2024-09-26 18:08:24 +0700

    aaaa

--- WebBanHangOnline/Controllers/AccountController.cs
+++ WebBanHangOnline/Controllers/AccountController.cs
@@ -305,7 +305,7 @@ namespace WebBanHangOnline.Controllers
         [HttpPost]
         [AllowAnonymous]
         [ValidateAntiForgeryToken]
-        public ActionResult ExternalLogin(string provider, string returnUrl)
+        public ActionResult ExternalLogin(string provider, string returnUrl)aasdad
         {
             // Request a redirect to the external login provider
             return new ChallengeResult(provider, Url.Action("ExternalLoginCallback", "Account", new { ReturnUrl = returnUrl }));
